89e9b3d5bd2b3d3737d45e63053e688f839f7b3f
문현건 <mhg1070@gmail.com>
2025-09-08 10:43:40 +0900

    initial commit

---
+++ src/main/java/com/kd/insuranceweb/InsuranceWebApplication.java
@@ -0,0 +1,13 @@
+package com.kd.insuranceweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InsuranceWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InsuranceWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kd/insuranceweb/InsuranceWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kd.insuranceweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InsuranceWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
