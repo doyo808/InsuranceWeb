9d3f7d99c82a95a7d3832a733077ae24da631de1
i7C-40 <i7C-40@DESKTOP-4JEE2Q3>
2025-09-24 11:39:41 +0900

    9.24

---
+++ src/main/java/com/kd/insuranceweb/claim/OcrSample.java
@@ -0,0 +1,44 @@
+package com.kd.insuranceweb.claim;
+
+import com.google.cloud.vision.v1.ImageAnnotatorClient;
+import com.google.cloud.vision.v1.AnnotateImageRequest;
+import com.google.cloud.vision.v1.AnnotateImageResponse;
+import com.google.cloud.vision.v1.Feature;
+import com.google.cloud.vision.v1.Image;
+import com.google.protobuf.ByteString;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.List;
+import java.util.ArrayList;
+
+public class OcrSample {
+    public static void main(String[] args) throws IOException {
+        // 이미지 파일 로드
+        String filePath = "C:/javaweb_yhs/InsuranceWebUploadedFiles/claims/1.png";
+        ByteString imgBytes = ByteString.readFrom(new FileInputStream(filePath));
+
+        // Vision API 요청
+        List<AnnotateImageRequest> requests = new ArrayList<>();
+        Image img = Image.newBuilder().setContent(imgBytes).build();
+        Feature feat = Feature.newBuilder().setType(Feature.Type.TEXT_DETECTION).build();
+        AnnotateImageRequest request =
+                AnnotateImageRequest.newBuilder().addFeatures(feat).setImage(img).build();
+        requests.add(request);
+        
+        
+        
+        try (ImageAnnotatorClient client = ImageAnnotatorClient.create()) {
+            AnnotateImageResponse response = client.batchAnnotateImages(requests).getResponses(0);
+            if (response.hasError()) {
+                System.out.println("Error: " + response.getError().getMessage());
+                return;
+            }
+
+            // OCR 결과 출력
+            System.out.println("=== OCR TEXT ===");            
+            System.out.println(response.getFullTextAnnotation().getText());
+            System.out.println(response.getFullTextAnnotation().getAllFields());
+            System.out.println(response.getFullTextAnnotation());
+        }
+    }
+}
