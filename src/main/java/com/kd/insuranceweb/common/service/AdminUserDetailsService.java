5fa2c15e666b94ed87f4e4d2aa115f5589df0fbd
문현건 <mhg1070@gmail.com>
2025-10-13 18:11:47 +0900

    * 관리자 로그인용 config 생성

---
+++ src/main/java/com/kd/insuranceweb/common/service/AdminUserDetailsService.java
@@ -0,0 +1,20 @@
+package com.kd.insuranceweb.common.service;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class AdminUserDetailsService implements UserDetailsService {
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
